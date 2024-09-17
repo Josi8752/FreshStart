ccc72d662359400317d8aee21b2e043075d1cf0f
Josiane Silva <josianesouza8752@gmail.com>
2024-09-17 19:02:53 -0300

    ex07

--- operadoresAritimeticos/ex/ex06.py
+++ operadoresAritimeticos/ex/ex06.py
@@ -8,3 +8,7 @@ triplo = (n1 ** (1/3))
 rQuadrada = math.sqrt(n1) # ou rQuadrada = (n1 ** (1/2))
 
 print (' O dobro de é: {} \n O triplo é: {:.1f}  \n A raiz quadrada é: {:.1f} '.format(dobro, triplo, rQuadrada))
+
+# print ('O dobro de {} vale {}'.format(n, (n*2)))
+# print ('O triplo de {} vale {}'.format(n, (n*3)))
+# print ('O tiplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3), n, pow(n, (1/2))))
---
+++ operadoresAritimeticos/ex/ex07.py
@@ -0,0 +1,13 @@
+import math
+
+
+#Média Aritimética
+
+aluno = input('Qual o nome do aluno? ')
+nota1 = float(input('Digite  a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+soma= (nota1 + nota2)
+mediaDasNotas = soma / 2
+
+print('A soma das notas é: {:.2f}. \nA média das notas é: {:.2f}'.format(soma, mediaDasNotas))
